8840b22f9071302020a155e1fc1fac6c9ac5cb0f
Mohamed el fadhel el hachemi <mohamedelfadhel.elhachemi@esprit.tn>
2017-02-17 22:52:59 +0100

    new Architecture

---
+++ client/app/app.component.js
@@ -0,0 +1,11 @@
+import template from './app.html';
+import './app.scss';
+import '../../node_modules/material-design-lite/material.min.css';
+
+
+let appComponent = {
+  template,
+  restrict: 'E'
+};
+
+export default appComponent;
