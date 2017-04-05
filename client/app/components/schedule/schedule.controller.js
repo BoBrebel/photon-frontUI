076461107f4ab01e54941766d01495915ba28da9
Hadh <soccerstar.laouini@gmail.com>
2017-04-05 01:23:42 +0100

    tried to add addshcedule module

--- client/app/components/schedule/schedule.controller.js
+++ client/app/components/schedule/schedule.controller.js
@@ -1,8 +1,12 @@
 class ScheduleController {
-  constructor(Schedule) {
+  constructor(Schedule,$scope,$http) {
     this.name = 'schedule';
+    this.$scope=$scope;
+    this.$http = $http;
     this.Schedule = Schedule;
     this.schedulelist = {};
+    this.$scope.newSchedule = {};
+
     this.Schedule.getAll().then((response)=>{
       this.schedulelist = response.data;
      console.log("LIST",this.schedulelist);
@@ -10,6 +14,28 @@ class ScheduleController {
 
     })
   }
+
+  formSubmit(date,from,to){
+    this.$scope.newSchedule = {
+      to: to,
+      from: from,
+      date:date,
+    };
+    console.log(this.$scope.newSchedule);
+    this.Schedule.addSchedule(this.$scope.newSchedule, this.$http).then((response)=>{
+      console.log(' Schedule Added :',response.data);
+    },(reject)=>{
+
+    });
+  }
+
+  /*formSubmit(){
+    this.Schedule.addSchedule().then((resposne)=>{
+      console.log(' Schedule Added :', resposne.data);
+    },(reject)=>{
+
+    })
+  }*/
 
   getAck() {
     console.log(this.Schedule.getAll());
--- client/app/services/schedule.service.js
+++ client/app/services/schedule.service.js
@@ -7,21 +7,24 @@ export default class Schedule {
     this.$http = $http;
     this.schedules = {};
   }
-
+  /*for now the user_id is static*/
   addSchedule(schedule, $http) {
     return $http({
       method: 'POST',
-      url: 'http://localhost:4000/schedules/user/58c00b4f44822044ec960706/schedule?to=Tozeur&from=Marsa&date=12-12-2012',
-      data: {
-        'schedule': schedule
-      }
+      url: 'http://localhost:4000/schedules/user/58c00b4f44822044ec960706/schedule?to='+
+      schedule.to+'&from='+schedule.from+'&date='+schedule.date
+      /*data: {
+        'to': schedule.to,
+        'from': schedule.from,
+        'date':schedule.date,
+        'user_id': '58c00b4f44822044ec960706'
+      }*/
     });
   }
   /*Returns the schedules obj after promise exec*/
   getScheduleList(){
     return this.schedules;
   }
-
   getAll() {
     return  this.$http({
         method:'GET',
