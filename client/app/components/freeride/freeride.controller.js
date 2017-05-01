f3b597a5c0ddd77353a92a444dcfef1c8a007867
Hadh <soccerstar.laouini@gmail.com>
2017-05-01 03:17:22 +0100

    check for ride controller method

--- client/app/components/freeride/freeride.controller.js
+++ client/app/components/freeride/freeride.controller.js
@@ -7,14 +7,22 @@ class FreerideController {
   }
 
   checkForRide(){
+    var test = {
+      "msg": "No Free Rides"
+    };
     this.Freeride.checkFreeRide().then((response) => {
       this.freeRide = response.data;
       console.log("Free ride", this.freeRide);
+      console.log("test", test);
+      /* need to check if the freeRide obj has a user, or has no free rides msg*/
+      /*if it has a user, we must either add money to his account or offer him a free ride ? */
+      /* if it has no free ride msg, display : You don't have free rides yet*/
 
     }, (reject) => {
       console.log('Something went wrong');
     });
   }
+
 }
 
 export default FreerideController;
